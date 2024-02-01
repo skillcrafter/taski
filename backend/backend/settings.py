fff8f773ac14d8a900d77c765bb16105f225850b
olga <olganigrova@yandex.ru>
2024-02-01 19:25:00 +0000

    1r

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,28 @@
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+# Скопируйте DSN из вашего личного кабинета на Sentry: 
+# Settings → Projects → <ваш-проект> → Client Keys (DSN).
+sentry_sdk.init(
+    # В этой переменной будет значение для вашего проекта.
+    dsn="https://8bb349be4a088403e2fad473c304ad9d@o4506638092730368.ingest.sentry.io/4506638103347200",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '158.160.28.213', '127.0.0.1', 'localhost', 'rtaskir.ddns.net'
+]
 
 
 # Application definition
@@ -103,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework import routers
-
 from api import views
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error), # Новый маршрут.
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
 ]
